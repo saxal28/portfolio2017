a2ef832f028d97881a9397ad95ab411a60e1c0bd
Alan Sax <saxal28@gmail.com>
2017-02-22 05:42:27 +0000

    added floating down arrow for better ui. also enabled back to start scrolling

--- public/src/main.js
+++ public/src/main.js
@@ -2,10 +2,21 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         controlArrows: false,
-        anchors:["home","aboutme","projects","contactme"]
+        anchors:["home","aboutme","projects","contactme"],
+        continuousVertical: true,
     });
 
+
+    console.log(window);
+    $("#down").click(function() {
+        // var down = document.getElementById("down");
+        // var hash = window.location.href.substring(41);
+        // var location = window.location;
+        // location.hash = "#aboutme";
+        // document.body.querySelector("#contact").scrollIntoView(true);
+    })
 });
 
 // fittext
  $("#responsive_headline").fitText();
+ 
